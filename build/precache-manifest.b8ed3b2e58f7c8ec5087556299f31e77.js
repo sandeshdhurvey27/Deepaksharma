ce5142eebf381cdda31f1ce42f1a06f6629d225c
sandeshdhurvey27 <sandeshdhurvey27061999@gmail.com>
2024-02-29 14:05:46 +0530

    first commit

---
+++ build/precache-manifest.b8ed3b2e58f7c8ec5087556299f31e77.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7faf7ba8cce6d5dee2e6",
+    "url": "/rachit_resume/static/css/main.0abd1cea.chunk.css"
+  },
+  {
+    "revision": "7faf7ba8cce6d5dee2e6",
+    "url": "/rachit_resume/static/js/main.7faf7ba8.chunk.js"
+  },
+  {
+    "revision": "5bfb743901ca7cdb5eaf",
+    "url": "/rachit_resume/static/css/1.140678dc.chunk.css"
+  },
+  {
+    "revision": "5bfb743901ca7cdb5eaf",
+    "url": "/rachit_resume/static/js/1.5bfb7439.chunk.js"
+  },
+  {
+    "revision": "0930cedb48f5259c0bdd",
+    "url": "/rachit_resume/static/js/runtime~main.0930cedb.js"
+  },
+  {
+    "revision": "7b11dbcdb562fad19e0ee1ffe1e71eee",
+    "url": "/rachit_resume/static/media/ab-img.7b11dbcd.png"
+  },
+  {
+    "revision": "5475aa4b3ed2fd6e7540959888b93c97",
+    "url": "/rachit_resume/static/media/surface.5475aa4b.jpeg"
+  },
+  {
+    "revision": "17a5b042bf658081b37655cac8b297c1",
+    "url": "/rachit_resume/static/media/vmcmachine.17a5b042.jpeg"
+  },
+  {
+    "revision": "9f1ceecbeab2b20acf0bc9b8245b974b",
+    "url": "/rachit_resume/static/media/drmachine.9f1ceecb.jpeg"
+  },
+  {
+    "revision": "f7eee13275c9f14b142934d2e89a59d1",
+    "url": "/rachit_resume/static/media/lathem.f7eee132.jpeg"
+  },
+  {
+    "revision": "a158324f8f5d819169d2f5dcc08d2535",
+    "url": "/rachit_resume/static/media/profile.a158324f.png"
+  },
+  {
+    "revision": "7117fc50902ab31ce31bb2e369d96bd3",
+    "url": "/rachit_resume/index.html"
+  }
+];
